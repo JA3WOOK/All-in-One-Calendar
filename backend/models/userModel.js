94506298b22945a0418342746cadee5350e62fb0
silk <lattepaw4@gmail.com>
2026-04-01 14:00:26 +0900

    userModel DB 로직 수정

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,9 +1,9 @@
 const pool = require("..config/db");
 
 // login_id로 사용자 조회 
-exports.findByLoginId = async (login_id) => {
+exports.getUserByLoginId = async (login_id) => {
    const sql = `
-      SELETE user_id, login_id, password, name
+      SELETE user_id, login_id, password, name, email, phone, department, profile_image
       FROM users
       WHERE logon_id = ?
    `;
