bed637fff4b59bc5129664d8a59e5c6f105bc0b7
silk <lattepaw4@gmail.com>
2026-04-02 16:32:21 +0900

    주석 수정

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,10 +2,10 @@ const authService = require("../services/authService");
 
 exports.login = async(req, res, next) => {
       try {
-         const email = req.body.login_id ? String(req.body.email).trim() : "";
+         const email = req.body.email ? String(req.body.email).trim() : "";
          const password = req.body.password ? String(req.body.password).trim() : "";
 
-         // 입력값 체크
+   
          if(!email || !password) {
             return res.status(400).json({
                   message: "아이디와 비밀번호를 입력하세요",
