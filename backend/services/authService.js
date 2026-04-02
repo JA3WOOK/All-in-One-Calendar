d9927c59e4bf2b92e5fdee13850a65f73feedb9e
silk <lattepaw4@gmail.com>
2026-04-02 16:28:49 +0900

    if문 수정

--- backend/services/authService.js
+++ backend/services/authService.js
@@ -1,36 +1,33 @@
-const bcrypt = require("bcrypt");
+
 const userModel = require("../models/userModel");
 const { createToken } = require("../utils/jwtUtil");
 
-exports.login = async (login_id, password) => {
-   const rows = await userModel.getUserByLoginId(login_id);
+exports.login = async (email, password) => {
 
-   if(!rows || rows.length === 0) {
-     const error = new Error("아이디 또는 비밀번호가 올바르지 않습니다.");
-     error.status = 401;
-     throw error;
-   }
-
-   const user = rows[0];
-
-
-   const isMatch = await bcrypt.compare(password, user.password);
-
-   if(!isMatch) {
-      const error = new Error("아이디 또는 비밀번호가 올바르지 않습니다.");
-      error.status = 401;
-      throw error;
-   }
+  const rows = await userModel.getUserByEmail(email);
   
-   const accessToken = createToken({
-      user_id: user.user_id,
-      login_id: user.login_id,
-   });
+  if (rows.length === 0) {
+    const error = new Error("조회된 사용자가 없습니다.");
+    error.status = 401;
+    throw error;
+  }
 
-   return {
-      message: "로그인 성공",
-      accessToken,
-      user: {
+  const user = rows[0];
+
+  if (password !== String(user.password).trim()) {
+    const error = new Error("비밀번호가 일치하지 않습니다.");
+    error.status = 401;
+    throw error;
+  }
+
+  const accessToken = createToken({
+    user_id: user.user_id,
+    email: user.email,
+  });
+
+  return {
+    accessToken,
+    user: {
       user_id: user.user_id,
       login_id: user.login_id,
       name: user.name,
@@ -38,6 +35,8 @@ exports.login = async (login_id, password) => {
       phone: user.phone,
       department: user.department,
       profile_image: user.profile_image,
-   },
-   };
+    },
+  };
 };
+
+
