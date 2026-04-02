74acfd53d034cb91c680293159dc88a4ed8de3cc
silk <lattepaw4@gmail.com>
2026-04-02 16:30:26 +0900

    에러 처리 수정

--- backend/utils/jwtUtil.js
+++ backend/utils/jwtUtil.js
@@ -1,16 +1,19 @@
 const jwt = require("jsonwebtoken");
 
+if (!process.env.JWT_SECRET) {
+  throw new Error("JWT_SECRET 환경변수 설정 안됨");
+}
+
 exports.createToken = (payload) => {
-   return jwt.sign(
-      payload,
-      process.env.JWT_SECRET,
-      {
-         expiresIn: process.env.JWT_EXPIRES_IN || "1h",
-      }
-   );
+  return jwt.sign(payload, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN || "1h",
+  });
 };
 
-
 exports.verifyToken = (token) => {
-   return jwt.verify(token, process.env.JWT_SELETE);
+  try {
+    return jwt.verify(token, process.env.JWT_SECRET);
+  } catch (err) {
+    return null;
+  }
 };
