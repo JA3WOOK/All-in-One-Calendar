69d3153d584eb3c2c3da98bd497ff8b45d71e077
silk <lattepaw4@gmail.com>
2026-04-02 13:20:27 +0900

    controller trim 수정

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,12 +1,21 @@
 const authService = require("../services/authService");
 
-exports.login = async(req, res) => {
+exports.login = async(req, res, next) => {
       try {
-         const { login_id, password } = req.body;
+         const login_id = req.body.login_id ? string(req.body.login_id).trim() : "";
+         const password = req.body.password ? string(req.body.password).trim() : "";
+
+         // 입력값 체크
+         if(!login_id || !password) {
+            return res.status(400).json({
+                  message: "아이디와 비밀번호를 입력하세요",
+            });
+         }
+
          const result = await authService.login(login_id, password);
 
          res.status(200).json(result);
       } catch (err) {
-         res.status(400).json({ message : err.message });
+         next(err);
       }
 };
