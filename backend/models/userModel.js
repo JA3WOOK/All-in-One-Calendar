05d8d1dc5d4ea5fea0bfe24ac15f56d6111bd243
silk <lattepaw4@gmail.com>
2026-04-02 16:23:36 +0900

    userModel 쿼리문 수정

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,13 +1,13 @@
 const pool = require("../config/db");
 
-// login_id로 사용자 조회 
-exports.getUserByLoginId = async (login_id) => {
+
+exports.getUserByEmail = async (email) => {
    const sql = `
       SELECT user_id, login_id, password, name, email, phone, department, profile_image
       FROM users
-      WHERE login_id = ?
+      WHERE email = ?
    `;
 
-   const [rows] = await pool.query(sql, [login_id]);
+   const [rows] = await pool.query(sql, [email]);
    return rows;
 };
