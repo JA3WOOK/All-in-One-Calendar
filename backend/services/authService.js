911f486404ea1e0ed5d0e1eeb7468ae86407c66b
silk <lattepaw4@gmail.com>
2026-04-02 13:32:28 +0900

    오타수정

--- backend/services/authService.js
+++ backend/services/authService.js
@@ -1,33 +1,43 @@
 const bcrypt = require("bcrypt");
 const userModel = require("../models/userModel");
-const { Profiler } = require("react");
+const { createToken } = require("../utils/jwtUtil");
 
 exports.login = async (login_id, password) => {
-   // 유저 조회
    const rows = await userModel.getUserByLoginId(login_id);
 
-   if(rows.length === 0) {
-      throw new Error("존재하지 않는 아이디입니다.");
+   if(!rows || rows.length === 0) {
+     const error = new Error("아이디 또는 비밀번호가 올바르지 않습니다.");
+     error.status = 401;
+     throw error;
    }
+
    const user = rows[0];
 
-   // 비밀번호 비교
+
    const isMatch = await bcrypt.compare(password, user.password);
 
    if(!isMatch) {
-      throw new Error("비밀번호가 일치하지 않습니다.");
+      const error = new Error("아이디 또는 비밀번호가 올바르지 않습니다.");
+      error.status = 401;
+      throw error;
    }
-   // 로그인 성공
+  
+   const accessToken = createToken({
+      user_id: user.user_id,
+      login_id: user.login_id,
+   });
+
    return {
-      user: {
       message: "로그인 성공",
+      accessToken,
+      user: {
       user_id: user.user_id,
       login_id: user.login_id,
       name: user.name,
       email: user.email,
       phone: user.phone,
       department: user.department,
-      Profile_image: user.Profile_image,
+      profile_image: user.profile_image,
    },
    };
 };
