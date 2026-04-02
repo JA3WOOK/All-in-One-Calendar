a30d0faa91b299a79335d9d3612c9c376b382555
silk <lattepaw4@gmail.com>
2026-04-02 16:20:28 +0900

    app.js 테스트 함수 삭제.

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,3 @@
-// app.js 에 연결 확인 코드 추가 
 const express = require("express"); 
 const cors = require("cors"); 
 const dotenv = require("dotenv");
@@ -8,34 +7,18 @@ dotenv.config();
 const pool = require("./config/db"); 
 const authRoutes = require("./routes/authRoutes");
 
-
 const app = express(); 
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
-    credentials: true,
-})
-); 
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  credentials: true,
+}));
 
-app.use(express.json()); 
-
-
-// DB 연결 테스트 
-async function testDB() { 
-    try { 
-        
-        const [rows] = await pool.query("SELECT 1"); 
-        console.log("DB 연결 성공!"); 
-
-    } catch (err) { 
-        console.error("DB 연결 실패:", err); 
-    } 
-} 
-
+app.use(express.json());
 
 // 기본 확인용
 app.get("/", (req, res) => { 
-    res.send("로그인 서버 실행 중" ); 
+  res.send("로그인 서버 실행 중"); 
 }); 
 
 // 로그인 라우터
@@ -43,31 +26,32 @@ app.use("/api/auth", authRoutes);
 
 // 등록되지 않은 경로 처리 
 app.use((req, res) => {
-    res.status(404).json({error : "요청한 경로를 찾을 수 없습니다."});
+  res.status(404).json({ error: "요청한 경로를 찾을 수 없습니다." });
 });
 
 // 전역 에러 처리 
 app.use((err, req, res, next) => {
-    console.error(err);
-    res.status(err.status || 500).json({
-        message: err.message || "서버 내부 오류가 발생했습니다.",
-    });
+  console.error(err);
+  res.status(err.status || 500).json({
+    message: err.message || "서버 내부 오류가 발생했습니다.",
+  });
 });
-
 
 const PORT = process.env.PORT || 3001; 
 
 async function startServer() {
-    try {
-        await pool.query("SELECT 1");
-        console.log(`DB 연결 성공`);
+  try {
+    await pool.query("SELECT 1"); // DB 연결 체크
+    console.log("DB 연결 성공");
 
-        app.listen(PORT, () => { 
-        console.log(`서버 실행: http://localhost:${PORT}`); 
-        }); 
-    } catch (err) {
-        console.error("DB 연결 실패:", err.message);
-        process.exit(1);
-    }
+    app.listen(PORT, () => { 
+      console.log(`서버 실행: http://localhost:${PORT}`); 
+    }); 
+  } catch (err) {
+    console.error("DB 연결 실패:", err.message);
+    process.exit(1);
+  }
 }
+
 startServer();
+
