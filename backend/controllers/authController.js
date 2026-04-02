51ab615415d20595a8be7cddf2adaef38bd02e57
silk <lattepaw4@gmail.com>
2026-04-02 13:54:02 +0900

    email 수정

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,17 +2,17 @@ const authService = require("../services/authService");
 
 exports.login = async(req, res, next) => {
       try {
-         const login_id = req.body.login_id ? string(req.body.login_id).trim() : "";
-         const password = req.body.password ? string(req.body.password).trim() : "";
+         const email = req.body.login_id ? String(req.body.email).trim() : "";
+         const password = req.body.password ? String(req.body.password).trim() : "";
 
          // 입력값 체크
-         if(!login_id || !password) {
+         if(!email || !password) {
             return res.status(400).json({
                   message: "아이디와 비밀번호를 입력하세요",
             });
          }
 
-         const result = await authService.login(login_id, password);
+         const result = await authService.login(email, password);
 
          res.status(200).json(result);
       } catch (err) {
