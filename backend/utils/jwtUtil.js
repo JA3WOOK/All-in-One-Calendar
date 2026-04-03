a7fce7c808e05655a80282755d4cc0cfc8c77858
silk <lattepaw4@gmail.com>
2026-04-03 15:17:16 +0900

    token 로직 수정

--- backend/utils/jwtUtil.js
+++ backend/utils/jwtUtil.js
@@ -1,18 +1,39 @@
 const jwt = require("jsonwebtoken");
 
-if (!process.env.JWT_SECRET) {
-  throw new Error("JWT_SECRET 환경변수 설정 안됨");
+if (!process.env.JWT_ACCESS_SECRET) {
+  throw new Error("JWT_ACCESS_SECRET 환경변수 설정 안됨");
 }
 
-exports.createToken = (payload) => {
-  return jwt.sign(payload, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN || "1h",
+if (!process.env.JWT_REFRESH_SECRET) {
+  throw new Error("JWT_REFRESH_SECRET 환경변수 설정 안됨");
+}
+
+// access token 생성
+exports.createAccessToken = (payload) => {
+  return jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {
+    expiresIn: process.env.JWT_ACCESS_EXPIRES_IN || "1h",
   });
 };
 
-exports.verifyToken = (token) => {
+// refresh token 생성
+exports.createRefreshToken = (payload) => {
+  return jwt.sign(payload, process.env.JWT_REFRESH_SECRET, {
+    expiresIn: process.env.JWT_REFRESH_EXPIRES_IN || "7d",
+  });
+};
+
+// access token 검증
+exports.verifyAccessToken = (token) => {
+    try {
+      return jwt.verify(token, process.env.JWT_ACCESS_SECRET);
+    } catch(err) {
+      return null;
+    }
+};
+
+exports.verifyRefreshToken = (token) => {
   try {
-    return jwt.verify(token, process.env.JWT_SECRET);
+    return jwt.verify(token, process.env.JWT_REFRESH_SECRET);
   } catch (err) {
     return null;
   }
