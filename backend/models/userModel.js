3adff927f68a2344c851aac64a0f1934a878184e
silk <lattepaw4@gmail.com>
2026-04-03 09:20:02 +0900

    쿼리 수정 및 매개변수 수정

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -3,7 +3,7 @@ const pool = require("../config/db");
 
 exports.getUserByEmail = async (email) => {
    const sql = `
-      SELECT user_id, login_id, password, name, email, phone, department, profile_image
+      SELECT user_id, password, name, email, phone, department, profile_image
       FROM users
       WHERE email = ?
    `;
@@ -11,3 +11,12 @@ exports.getUserByEmail = async (email) => {
    const [rows] = await pool.query(sql, [email]);
    return rows;
 };
+
+exports.createUser = async (login_id, name, email, password) => {
+   const sql = `
+   INSERT INTO users (name, email, password)
+   VALUES(?, ?, ?, ?)`;
+
+   const [result] = await pool.query(sql, [name, email, password]);
+   return result;
+}
