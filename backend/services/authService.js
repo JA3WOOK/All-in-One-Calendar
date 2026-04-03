c067d5921ec7498decbd11d2facee1f900c5cdab
silk <lattepaw4@gmail.com>
2026-04-03 15:17:51 +0900

    token 로직 분리

--- backend/services/authService.js
+++ backend/services/authService.js
@@ -1,9 +1,16 @@
 const bcrypt = require("bcrypt");
 const userModel = require("../models/userModel");
-const { createToken } = require("../utils/jwtUtil");
+
+
+const {
+  createAccessToken,
+  createRefreshToken,
+} = require("../utils/jwtUtil");
+
+const refreshTokenModel = require("../models/refreshTokenModel");
 
 // 로그인
-exports.login = async (email, password) => {
+exports.login = async (email, password, ip) => {
   const rows = await userModel.getUserByEmail(email);
 
   // 조회 결과 확인 로그 
@@ -18,7 +25,7 @@ exports.login = async (email, password) => {
 
   const user = rows[0];
 
-  // [추가] 조회된 사용자 확인
+  // 조회된 사용자 확인
   console.log("login user:", user);
 
 
@@ -30,7 +37,7 @@ exports.login = async (email, password) => {
 
   const isMatch = await bcrypt.compare(password, user.password);
 
-   // [추가] 비밀번호 비교 결과 확인
+   //비밀번호 비교 결과 확인
   console.log("password match:", isMatch);
 
   if (!isMatch) {
@@ -39,14 +46,33 @@ exports.login = async (email, password) => {
     throw error;
   }
 
-  const accessToken = createToken({
+  const accessToken = createAccessToken({
     user_id: user.user_id,
     email: user.email,
   });
 
+   // refresh token 생성
+  const refreshToken = createRefreshToken({
+    user_id: user.user_id,
+    email: user.email,
+  });
+
+  // refresh token 만료 시간 (7일)
+  const expiryDate = new Date();
+  expiryDate.setDate(expiryDate.getDate() + 7);
+
+  // DB에 refresh token 저장 
+    await refreshTokenModel.create(
+    user.user_id,
+    refreshToken,
+    expiryDate,
+    ip || null
+  );
+
   return {
     message: "로그인 성공",
     accessToken,
+    refreshToken,
     user: {
       user_id: user.user_id,
       name: user.name,
@@ -78,3 +104,5 @@ exports.signup = async (name, email, password) => {
     user_id: result.insertId,
   };
 };
+
+
