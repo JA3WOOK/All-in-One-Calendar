e69ae9b5db21a8c92778f516b5ff683168e5f5a1
JaewookBae <cook_jw@naver.com>
2026-04-05 15:22:03 +0900

    feat:env 보안 설정 및 gitignore 업데이트

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,20 +1,17 @@
-//  MySQL 연결 설정
+require("dotenv").config();
+const mysql = require("mysql2/promise");
 
-const mysql = require("mysql2/promise");  // Promise + async/await 사용  
- 
-// 커넥션 풀 생성 
-const pool = mysql.createPool({ 
-  host: "localhost",     // DB 서버 
-  user: "root",          // 계정 
-  password: "1234",      // 비밀번호 
-  database: "all_in_one",     // DB명
-  waitForConnections: true, // 기다릴 지 유무  
-  connectionLimit: 10, //동시 연결 수   
-  queueLimit: 0,   // queue에 쌓을 수 있는 요청 개수 제한,  0은 무한대기
+const pool = mysql.createPool({
+  host:             process.env.DB_HOST,
+  user:             process.env.DB_USER,
+  password:         process.env.DB_PASSWORD,
+  database:         process.env.DB_NAME,
+  port:             process.env.DB_PORT || 3306,
+  waitForConnections: true,
+  connectionLimit:  10,
+  queueLimit:       0,
+  timezone:         "+09:00",
+  dateStrings:      true,
+});
 
-  // 자동미루기 UTC 시간 맞추기
-  timezone: "+09:00",
-  dateStrings: true,
-}); 
- 
 module.exports = pool;
